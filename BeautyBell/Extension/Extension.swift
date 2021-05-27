b4b2b83a0f7c1e4dc178c8b83283cf5e4f61836e
Randy Efan Jayaputra <randyrandyrej8@gmail.com>
2021-05-27 22:23:33 +0700

    add extension for uiimageview

---
+++ BeautyBell/Extension/Extension.swift
@@ -0,0 +1,22 @@
+//
+//  Extension.swift
+//  BeautyBell
+//
+//  Created by Randy Efan Jayaputra on 27/05/21.
+//
+
+import UIKit
+
+extension UIImageView {
+    func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
+    }
+}
