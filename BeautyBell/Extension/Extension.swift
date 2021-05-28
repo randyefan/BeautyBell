5f86c1ec1dfab70a36b769bd0c25689637ae88a2
Randy Efan Jayaputra <randyrandyrej8@gmail.com>
2021-05-28 13:29:49 +0700

    add func makerounded to extension

--- BeautyBell/Extension/Extension.swift
+++ BeautyBell/Extension/Extension.swift
@@ -19,6 +19,14 @@ extension UIImageView {
             }
         }
     }
+    
+    func makeRounded() {
+        self.layer.borderWidth = 0
+        self.layer.masksToBounds = false
+        self.layer.borderColor = UIColor.black.cgColor
+        self.layer.cornerRadius = self.frame.height / 2
+        self.clipsToBounds = true
+    }
 }
 
 extension String{
